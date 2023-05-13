1fdb86fe8117d5bb54b2faefad3233033cd3612b
Caio <caiovitordias1@gmail.com>
2023-05-13 20:40:36 -0300

    adicionando estilizacao

---
+++ atlantisinterface/src/components/header.jsx
@@ -0,0 +1,11 @@
+import "../styles/header.css"
+
+export default function Header(){
+    return(
+        <>
+            <div className="header">
+                <h1 className="logo">ATLANTIS</h1>
+            </div>
+        </>
+    )
+}
--- atlantisinterface/src/pages/home.jsx
+++ atlantisinterface/src/pages/home.jsx
@@ -1,17 +1,42 @@
+import { useNavigate } from "react-router-dom";
+import Header from "../components/header";
 import ModalCadastro from "../components/modalCadastro";
+import "../styles/home.css"
 
 export default function Home() {
-    
+    const navigate = useNavigate()
+
+    function handle1(e){
+        e.preventDefault()
+        navigate("/cadastrarCliente")
+    }
+    function handle2(e){
+        e.preventDefault()
+        navigate("/cadastrarDependente")
+    }
+    function handle3(){
+
+    }
+    function handle4(){
+
+    }
     return(
         <div>
-            <button type="button" className="btn btn-primary">Cadastrar Cliente</button>
-            <button type="button" className="btn btn-primary">Cadastrar Dependentes</button>
-            <button type="button" className="btn btn-primary">Listar Clientes</button>
-            <button type="button" className="btn btn-primary">Excluir Cliente</button>
-
-            
-
-
+            <Header />
+            <div className="textos">
+                <h1>
+                    Bem Vindo ao Atlantis!
+                </h1>
+                <h2>
+                    Escolha uma funcionalidade:
+                </h2>
+            </div>
+            <div className="menuPrincipal d-flex flex-column justify-content-center align-items-center">
+                <button type="button" className="btn btn-primary" onClick={(e) => handle1(e)}>Cadastrar Cliente</button>
+                <button type="button" className="btn btn-primary" onClick={(e) => handle2(e)}>Cadastrar Dependentes</button>
+                <button type="button" className="btn btn-primary">Listar Clientes</button>
+                <button type="button" className="btn btn-primary">Excluir Cliente</button>
+            </div>
         </div>
     )
 }
